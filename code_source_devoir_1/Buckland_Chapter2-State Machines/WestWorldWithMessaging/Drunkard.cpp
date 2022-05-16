b411ed6eb90f60357d02efcbf6046593753df097
IQUELFergal <fergal.iquel@gmail.com>
2022-05-16 17:04:05 -0400

    Update Drunkard.cpp

---
+++ code_source_devoir_1/Buckland_Chapter2-State Machines/WestWorldWithMessaging/Drunkard.cpp
@@ -0,0 +1,14 @@
+#include "Drunkard.h"
+
+bool Drunkard::HandleMessage(const Telegram& msg)
+{
+	return m_pStateMachine->HandleMessage(msg);
+}
+
+
+void Drunkard::Update()
+{
+	SetTextColor(FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+
+	m_pStateMachine->Update();
+}
